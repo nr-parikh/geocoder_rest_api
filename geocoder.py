568e228c301aad8d75957d6c79938ba5d7d446e4
Neel Parikh <nrparikh1992@gmail.com>
2018-02-17 21:17:08 -0500

    [Impl] Added functionality of getting URL Defined two more functions that retrieve the URL of the location on the map.

--- geocoder.py
+++ geocoder.py
@@ -8,8 +8,10 @@ class Geocoder():
 		self.here_app_id_ = 'Mnswcv5a6ivjZ2XGDR4s'
 		self.here_app_code_ = 'EZ1qqKVhs452aUBKdUe1Gg'
 
-		self.google_geocode_url_ = 'https://maps.googleapis.com/maps/api/geocode/json'
-		self.here_geocode_url_ = 'https://geocoder.cit.api.here.com/6.2/geocode.json'
+		self.google_geocode_url_ = "https://maps.googleapis.com/maps/api/geocode/json"
+		self.google_places_url_ = "https://maps.googleapis.com/maps/api/place/details/json"
+		self.here_geocode_url_ = "https://geocoder.cit.api.here.com/6.2/geocode.json"
+		self.here_places_url_ = "https://wego.here.com/?x=ep&map="		
 
 	def googleGeocoder(self, search=""):
 
@@ -29,6 +31,25 @@ class Geocoder():
 		response_json = json.loads(data.decode(encoding))
 
 		return response_json	
+
+	def googlePlace(self, place_id=""):
+
+		values = dict()
+		values['key'] = self.google_api_key_
+		values['place_id'] = place_id
+
+		payload = urllib.parse.urlencode(values)
+
+		complete_url = self.google_places_url_ + '?' + payload
+
+		response = urllib.request.urlopen(complete_url)
+		data = response.read()
+
+		encoding = response.info().get_content_charset('utf-8')
+
+		response_json = json.loads(data.decode(encoding))
+
+		return response_json		
 
 	def hereGeocoder(self, search=""):
 
@@ -50,20 +71,36 @@ class Geocoder():
 
 		return response_json		
 
+	def herePlace(self, lat="", lng=""):
+
+		complete_url = self.here_places_url_ + lat + ',' + lng + ',' + '14'
+
+		return complete_url
+
 
 if __name__ == "__main__":
 
 	geocdr = Geocoder()
 
-	print("Enter the place to be searched:")
-	query = input()
+	query = "San Francisco"
 	response_json = geocdr.googleGeocoder(str(query))
+	place_id = response_json["results"][0]["place_id"]
 	position = response_json['results'][0]['geometry']['location']
+	url = geocdr.googlePlace(str(place_id))["result"]["url"]
 
 	print("\n{} is located at: ".format(query))
 	print(position)
+	print("Visit the URL below to open maps:")
+	print(url)
+	print("Found using Google geocoder API.\n")
 
 	response_json = geocdr.hereGeocoder(str(query))
 	position = response_json['Response']['View'][0]['Result'][0]['Location']['DisplayPosition']
+	lat = position['Latitude']
+	lng = position['Longitude']
+	url = geocdr.herePlace(lat=str(lat), lng=str(lng))
 
 	print(position)
+	print("Visit the URL below to open maps:")
+	print(url)
+	print("Found using Here geocoder API.")	
